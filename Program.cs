7bef2240bb742f9cd142362ba231eb5ed9edc96c
VlShost <vlshost9386@gmail.com>
2024-07-06 13:30:47 +0300

    Basic UI realization
    Implements basic user flow to start or end program execution.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace shapes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("*--------------------*");
+            Console.WriteLine("*       SHAPES       *");
+            Console.WriteLine("*--------------------*");
+
+            while (!endApp)
+            {
+                Console.WriteLine("Press Enter to start or Esc to exit");
+
+                ConsoleKeyInfo inputKey = Console.ReadKey(true);
+                if (inputKey.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Enter the name of shape (rectangle or square):");
+
+                    string input = Console.ReadLine();
+                    Console.WriteLine($"You entered {input}");
+                }
+                else if (inputKey.Key == ConsoleKey.Escape)
+                {
+                    endApp = true;
+                }
+            }
+        }
+    }
+}
