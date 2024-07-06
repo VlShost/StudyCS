e550cd131986c0aed479d987b52dc0041123275b
VlShost <vlshost9386@gmail.com>
2024-07-06 15:16:11 +0300

    Add rectangle shape

---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace shapes
+{
+    internal class Rectangle
+    {
+        public int SideX { get; set; }
+        public int SideY { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            SideX = width;
+            SideY = height;
+        }
+
+        public int GetArea()
+        {
+            return SideX * SideY;
+        }
+
+        public int GetPerimeter()
+        {
+            return (SideX + SideY) * 2;
+        }
+    }
+}
