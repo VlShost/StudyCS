ab235bc327a77d99972dc56360631ed4395c586b
VlShost <vlshost9386@gmail.com>
2024-07-06 15:20:16 +0300

    Add square shape

---
+++ Square.cs
@@ -0,0 +1,22 @@
+﻿namespace shapes
+{
+    public class Square
+    {
+        public int Side { get; set; }
+
+        public Square(int side)
+        {
+            Side = side;
+        }
+
+        public int GetArea()
+        {
+            return Side * Side;
+        }
+
+        public int GetPerimeter()
+        {
+            return 4 * Side;
+        }
+    }
+}
