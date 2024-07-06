9a33b8e4dbd340dedb8216da2a9e93b26b2ee62a
VlShost <vlshost9386@gmail.com>
2024-07-06 16:52:00 +0300

    Add input validation for negative and non-integer numbers

--- ShapeHandler.cs
+++ ShapeHandler.cs
@@ -7,14 +7,48 @@
             IArea area = null;
             IPerimeter perimeter = null;
 
+            bool validInput = false;
+
             switch (shapeName.ToLower())
             {
                 case "rectangle":
-                    Console.WriteLine("Enter WIDTH of rectangle:");
-                    int width = int.Parse(Console.ReadLine());
+                    int width, height;
 
-                    Console.WriteLine("Enter HEIGHT of rectangle:");
-                    int height = int.Parse(Console.ReadLine());
+                    do
+                    {
+                        Console.WriteLine("Enter WIDTH of rectangle:");
+
+                        if (!int.TryParse(Console.ReadLine(), out width))
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid integer.");
+                        }
+                        else if (width <= 0)
+                        {
+                            Console.WriteLine("Invalid input. Please enter a positive integer.");
+                        }
+                        else
+                        {
+                            validInput = true;
+                        }
+                    } while (!validInput);
+
+                    do
+                    {
+                        Console.WriteLine("Enter HEIGHT of rectangle:");
+
+                        if (!int.TryParse(Console.ReadLine(), out height))
+                        {
+                            Console.WriteLine("Invalid input. Please enter an integer.");
+                        }
+                        else if (height <= 0)
+                        {
+                            Console.WriteLine("Invalid input. Height must be a positive integer.");
+                        }
+                        else
+                        {
+                            validInput = true;
+                        }
+                    } while (!validInput);
 
                     Rectangle rectangle = new Rectangle(width, height);
 
@@ -23,8 +57,24 @@
                     break;
 
                 case "square":
-                    Console.WriteLine("Enter side length of square:");
-                    int side = int.Parse(Console.ReadLine());
+                    int side;
+
+                    do
+                    {
+                        Console.WriteLine("Enter side length of square:");
+                        if (!int.TryParse(Console.ReadLine(), out side))
+                        {
+                            Console.WriteLine("Invalid input. Please enter an integer.");
+                        }
+                        else if (side <= 0)
+                        {
+                            Console.WriteLine("Invalid input. Side must be a positive integer.");
+                        }
+                        else
+                        {
+                            validInput = true;
+                        }
+                    } while (!validInput);
 
                     Square square = new Square(side);
 
@@ -33,7 +83,7 @@
                     break;
 
                 default:
-                    Console.WriteLine("Unknown shape");
+                    Console.WriteLine("Invalid shape name");
                     return;
             }
 
