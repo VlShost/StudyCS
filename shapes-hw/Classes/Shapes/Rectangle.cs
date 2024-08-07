2fa564a6689ae3108ef34700c30d326424ddb3e4
VlShost <vlshost9386@gmail.com>
2024-08-07 13:13:48 +0300

    improved rectangle class accessors

--- shapes-hw/Classes/Shapes/Rectangle.cs
+++ shapes-hw/Classes/Shapes/Rectangle.cs
@@ -4,13 +4,25 @@ namespace shapes_hw.Classes.Shapes
 {
     public class Rectangle : IShapeAreaMeter, IShapePerimeterMeter
     {
-        public int SideX { get; set; }
-        public int SideY { get; set; }
+        public int SideX { get; init; }
+        public int SideY { get; init; }
+        public int Area { get; private set; }
+        public int Perimeter { get; private set; }
+
+        public Rectangle()
+        {
+            SideX = 0;
+            SideY = 0;
+            Area = 0;
+            Perimeter = 0;
+        }
 
         public Rectangle(int width, int height)
         {
             SideX = width;
             SideY = height;
+            Area = GetArea();
+            Perimeter = GetPerimeter();
         }
 
         public int GetArea()
