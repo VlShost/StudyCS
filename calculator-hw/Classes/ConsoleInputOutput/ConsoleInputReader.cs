a6430203c3dd1d181f71245dfdcf25416acc3fae
VlShost <vlshost9386@gmail.com>
2024-08-11 16:21:33 +0300

    add Console IO classes

---
+++ calculator-hw/Classes/ConsoleInputOutput/ConsoleInputReader.cs
@@ -0,0 +1,17 @@
+﻿using calculator_hw.Interfaces;
+
+namespace calculator_hw.Classes.ConsoleInputOutput
+{
+    public class ConsoleInputReader : IInputProvider
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public string ReadKey()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ calculator-hw/Classes/ConsoleInputOutput/ConsoleInputWriter.cs
@@ -0,0 +1,17 @@
+﻿using calculator_hw.Interfaces;
+
+namespace calculator_hw.Classes.ConsoleInputOutput
+{
+    public class ConsoleInputWriter : IOutputProvider
+    {
+        public void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        public void Write(string output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
