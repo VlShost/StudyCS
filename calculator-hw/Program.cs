58c6c7fe79e2a66575ca90720190eaa257b7157d
VlShost <vlshost9386@gmail.com>
2024-09-05 12:49:44 +0300

    refactored UI

--- calculator-hw/Program.cs
+++ calculator-hw/Program.cs
@@ -1,5 +1,6 @@
 ﻿using calculator_hw.Classes;
 using calculator_hw.Classes.ConsoleInputOutput;
+using calculator_hw.Enums;
 using calculator_hw.Interfaces;
 
 namespace calculator_hw
@@ -14,46 +15,88 @@ namespace calculator_hw
             Console.WriteLine("* Console Calculator *");
             Console.WriteLine("*--------------------*");
 
+            Console.WriteLine("\nSupported Operations:");
+            Console.WriteLine("  +  Addition          Example: 2 + 2 =");
+            Console.WriteLine("  -  Subtraction       Example: 5 - 3 =");
+            Console.WriteLine("  *  Multiplication    Example: 4 * 7 =");
+            Console.WriteLine("  /  Division          Example: 10 / 2 =");
+
+            Console.WriteLine("\nHow to Use:");
+            Console.WriteLine("  - Enter Numbers and Operators. Multiple operations allowed. Example: 2 + 2 + 2");
+            Console.WriteLine("  - Enter Decimal Numbers: Use '.' or ',' to input decimals.");
+            Console.WriteLine("  - Complete the Operation: Press = to calculate the result. Example: 2 + 2 = 4");
+            Console.WriteLine("  - Delete Last Character: Press Backspace to remove the last input.");
+            Console.WriteLine("  - Exit the Program: Press Esc at any time to close the app.");
+
             IInputProvider inputProvider = new ConsoleInputReader();
             IOutputProvider outputProvider = new ConsoleInputWriter();
 
             var calculator = new Calculator(outputProvider);
 
-            char inputKey = Char.MinValue;
-            string input = "";
-
             while (!endApp)
             {
-                Console.Write("\nType a desired operation: ");
+                Console.Write("\nPress Enter to start or Esc to exit");
 
-                do
+                ConsoleKeyInfo selection = Console.ReadKey(true);
+
+                if (selection.Key == ConsoleKey.Enter)
                 {
-                    inputKey = Console.ReadKey().KeyChar;
+                    Console.Write("\nType a desired operation: ");
 
-                    if (inputKey == '=')
+                    string input = "";
+
+                    while (true)
                     {
-                        break;
-                    }
-                    else if (inputKey == 'q')
-                    {
-                        endApp = true;
-                        break;
-                    }
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-                    input += inputKey;
-                } while (true);
+                        if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            endApp = true;
+                            break;
+                        }
 
-                if (inputKey == '=')
-                {
-                    //InputReader.Read(input);
-                    input = "";
+                        char inputKey = keyInfo.KeyChar;
+
+                        if (char.IsDigit(inputKey) ||
+                            (inputKey == '.' || inputKey == ',') ||
+                            Enum.IsDefined(typeof(Operators), (int)inputKey) ||
+                            inputKey == '=' ||
+                            inputKey == ' ')
+                        {
+                            Console.Write(inputKey);
+                            input += inputKey;
+                        }
+
+                        if (keyInfo.Key == ConsoleKey.Backspace)
+                        {
+                            if (input.Length > 0)
+                            {
+                                input = input.Remove(input.Length - 1);
+                                Console.Write("\b \b");
+                            }
+                        }
+
+                        if (inputKey == '=')
+                        {
+                            Console.WriteLine($"\n{input}");
+
+                            var tokenizedInput = InputTokenizer.Tokenize(input);
+
+                            foreach (var tokenItem in tokenizedInput)
+                            {
+                                Console.WriteLine($"{tokenItem}");
+                            }
+
+                            Console.WriteLine("\nCount: " + tokenizedInput.Count);
+
+                            input = "";
+                        }
+                    };
                 }
-                else if (endApp)
+                else if (selection.Key == ConsoleKey.Escape)
                 {
-                    break;
+                    endApp = true;
                 }
-
-                Console.WriteLine("\nPress \"Enter\" to start new operation or press \"q\" to close the app:");
             }
         }
     }
