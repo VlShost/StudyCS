ce08c5173fd2b561e3345e2c48ff016292450641
VlShost <vlshost9386@gmail.com>
2024-09-06 15:57:01 +0300

    add priority processor class

---
+++ calculator-hw/Classes/OperationPriorityProcessor.cs
@@ -0,0 +1,12 @@
+﻿namespace calculator_hw.Classes
+{
+    public class OperationPriorityProcessor
+    {
+        private List<string> tokens;
+
+        public OperationPriorityProcessor(List<string> inputTokens)
+        {
+            tokens = inputTokens;
+        }
+    }
+}
