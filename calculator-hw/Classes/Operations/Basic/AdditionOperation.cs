54af3e3f907887f752f3cecaa85a0a99e5951dbb
VlShost <vlshost9386@gmail.com>
2024-09-11 18:46:59 +0300

    small fix

--- calculator-hw/Classes/Operations/Basic/AdditionOperation.cs
+++ calculator-hw/Classes/Operations/Basic/AdditionOperation.cs
@@ -4,9 +4,9 @@ namespace calculator_hw.Classes.Operations.Basic
 {
     public class AdditionOperation : IOperation
     {
-        public double Execute(double leftOperand, double rightOpetrand)
+        public double Execute(double leftOperand, double rightOperand)
         {
-            return leftOperand += rightOpetrand;
+            return leftOperand += rightOperand;
         }
     }
 }
