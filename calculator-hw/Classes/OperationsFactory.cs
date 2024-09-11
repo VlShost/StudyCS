5e9e9af3567959a0fa9046cb68c41db94d16b027
VlShost <vlshost9386@gmail.com>
2024-09-11 21:36:33 +0300

    add factory for operations

---
+++ calculator-hw/Classes/OperationsFactory.cs
@@ -0,0 +1,30 @@
+﻿using calculator_hw.Classes.Operations.Basic;
+using calculator_hw.Enums;
+using calculator_hw.Interfaces;
+
+namespace calculator_hw.Classes
+{
+    public class OperationsFactory
+    {
+        public static IOperation CreateBasicOperation(Operators operators)
+        {
+            switch (operators)
+            {
+                case Operators.Add:
+                    return new AdditionOperation();
+
+                case Operators.Subtract:
+                    return new SubtractionOperation();
+
+                case Operators.Multiply:
+                    return new MultiplicationOperation();
+
+                case Operators.Divide:
+                    return new DivisionOperation();
+
+                default:
+                    throw new NotImplementedException($"{operators} currently not supported");
+            }
+        }
+    }
+}
